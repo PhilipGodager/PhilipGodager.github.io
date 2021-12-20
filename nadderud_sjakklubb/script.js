78efc17823cb5bc99e6c18095c7722b64e64f74d
PhilipGodager <92916853+PhilipGodager@users.noreply.github.com>
2021-12-20 11:28:59 +0100

    Add files via upload

--- nadderud_sjakklubb/script.js
+++ nadderud_sjakklubb/script.js
@@ -4,9 +4,19 @@ const navbarLinks = document.getElementsByClassName('meny-lenker')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
-
+/*
 function myFunction1() {
-    var x = document.getElementById("løsning1");
+  var x = document.getElementById("løsning1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+*/
+
+  function myFunction(divid) {
+    var x = document.getElementById(divid);
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
@@ -14,11 +24,7 @@ function myFunction1() {
     }
   }
 
-  function myFunction2() {
-    var x = document.getElementById("løsning2");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
+  // Slå av "bryteren"
+  myFunction(`løsning1`);
+  myFunction(`løsning2`);
+  
