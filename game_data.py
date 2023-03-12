fea3b958314bafb5214c4375ebb07df2b23e5ace
PhilipGodager <92916853+PhilipGodager@users.noreply.github.com>
2023-03-12 23:56:34 +0100

    Delete game_data.py

--- game_data.py
+++
@@ -1,7 +0,0 @@
-level_0 = {
-    "terrain" : "tiled/level_0_terrain.csv",
-    "coins" : "tiled/level_0_coins.csv",
-    "grass" : "tiled/level_0_grass.csv",
-    "player" : "tiled/level_0_player.csv",
-    "spikes" : "tiled/level_0_spikes.csv"
-    }
