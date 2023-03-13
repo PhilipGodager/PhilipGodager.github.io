1d2abcb5bbfbfd4004db51046a6f4248859efb1c
PhilipGodager <92916853+PhilipGodager@users.noreply.github.com>
2023-03-13 01:55:59 +0100

    Add files via upload

---
+++ mage_knight/tiled/spikes.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="spikes" tilewidth="65" tileheight="83" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="65" height="83" source="../bilder/objekter/spikes.png"/>
+ </tile>
+ <tile id="1">
+  <image width="65" height="61" source="../bilder/objekter/spikes.png"/>
+ </tile>
+</tileset>
